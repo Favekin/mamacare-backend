be50cabb6eda960ca629e25182889761f956a63f
Favekin <nwoyechukwunonsokingsley@gmail.com>
2025-12-12 16:22:19 +0100

    second

--- server.js
+++ server.js
@@ -21,5 +21,10 @@ app.use(
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/ai", require("./routes/ai"));
 
+app.get("/", (req, res) => {
+  res.send("Backend running successfully");
+});
+
+
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
